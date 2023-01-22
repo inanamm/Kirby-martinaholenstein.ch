ed198772f994fb21b82abbd1ceeb7aff69f37f03
inanamm <martina.holenstein@gmail.com>
2023-01-22 13:46:20 +0100

    fixed about page fixed projects redirect

--- site/templates/about.php
+++ site/templates/about.php
@@ -5,29 +5,27 @@
 
 <?php snippet('head') ?>
 
-<body id="about-page" data-barba="wrapper">
-    <div data-barba="container" data-barba-namespace="about">
-        <header>
-            <nav>
-                <a href="<?= $pages->get('home')->url()?>"><?= $pages->get('projects')->title() ?></a>
-                <h1><?= $page->title() ?></h1>
-            </nav>
-        </header>
+<body id="about-page">
+    <header>
+        <nav>
+            <a href="<?= $pages->get('home')->url()?>"><?= $pages->get('projects')->title() ?></a>
+            <h1><?= $page->title() ?></h1>
+        </nav>
+    </header>
 
-        <main>
-            <p><?= $page->text() ?></p>
+    <main>
+        <p><?= $page->text() ?></p>
+        <br>
+        <p>
+            <?php if ($contact = $page->contact()->toObject()): ?>
+            <a href="mailto:<?= $contact->email() ?>"><?= $contact->text() ?></a>
+            <?php endif ?>
             <br>
-            <p>
-                <?php if ($contact = $page->contact()->toObject()): ?>
-                <a href="mailto:<?= $contact->email() ?>"><?= $contact->text() ?></a>
-                <?php endif ?>
-                <br>
-                <?php if ($instagram = $page->instagram()->toObject()): ?>
-                <a href="<?= $instagram->link() ?>" target="_blank"><?= $instagram->text() ?></a>
-                <?php endif ?>
-            </p>
-        </main>
-    </div>
+            <?php if ($instagram = $page->instagram()->toObject()): ?>
+            <a href="<?= $instagram->link() ?>" target="_blank"><?= $instagram->text() ?></a>
+            <?php endif ?>
+        </p>
+    </main>
 </body>
 
 </html>
---
+++ site/templates/projects.php
@@ -0,0 +1,1 @@
+<?php go($pages->get('home')->url(), 301) ?>
