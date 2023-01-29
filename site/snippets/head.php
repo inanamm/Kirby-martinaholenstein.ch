d2f4eb83d332a24dace806570dd77b8fefbc8fa7
inanamm <martina.holenstein@gmail.com>
2023-01-29 14:39:13 +0100

    corrected css path and added favicons

--- site/snippets/head.php
+++ site/snippets/head.php
@@ -2,6 +2,15 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?= $site->title() ?></title>
-    <?= css('assets/css/main.css') ?>
-    <link rel="icon" type="image/png" href="assets/icon2.png">
+    <?= css(['assets/css/_reset.css', 'assets/css/main.css']) ?>
+
+    <!-- favicon -->
+    <link rel="apple-touch-icon" sizes="180x180" href="<?= url('favicon/apple-touch-icon.png') ?>">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?= url('favicon/favicon-32x32.png') ?>">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?= url('favicon/favicon-16x16.png') ?>">
+    <link rel="manifest" href="<?= url('favicon/site.webmanifest') ?>">
+    <link rel="mask-icon" href="<?= url('favicon/safari-pinned-tab.svg') ?>" color="#ff0000">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
+    
 </head>
