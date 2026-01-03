48f79551bddb9b91c0f2baec351820e0c6d4077c
inanamm <62645824+inanamm@users.noreply.github.com>
2026-01-03 10:50:16 +0100

    add new tracking

--- site/snippets/head.php
+++ site/snippets/head.php
@@ -12,5 +12,9 @@
     <link rel="mask-icon" href="<?= url('favicon/safari-pinned-tab.svg') ?>" color="#ff0000">
     <meta name="msapplication-TileColor" content="#da532c">
     <meta name="theme-color" content="#ffffff">
-    <script defer src="https://umami.monsteralab.xyz/script.js" data-website-id="25eec6c6-cf19-4ab3-8005-948b812ce985"></script>
+
+    <?php if (option('analytics')): ?>
+        <script defer src="https://cloud.umami.is/script.js" data-website-id="b7267b35-a872-4630-9954-c41868867eb2"></script>
+    <?php endif ?>
+
 </head>
